3f3bfb0040c8d2635ebec9e6bfdcdd6112ece22b
AndyChoi <AndyChoi4495@github.com>
2023-10-26 13:05:24 -0400

    update next.config

--- client/next.config.js
+++ client/next.config.js
@@ -1,10 +1,9 @@
 /** @type {import('next').NextConfig} */
-const { nextConfig } = require('next-images');
 
 const nextConfig = {
     reactStrictMode: true,
     images: {
-        domains: ['www.gravatar.com', 'localhost', 'ec2-18-117-131-28.us-east-2.compute.amazonaws.com:4000'],
+        domains: ['www.gravatar.com', 'localhost', 'ec2-18-117-131-28.us-east-2.compute.amazonaws.com'],
     },
 };
 
